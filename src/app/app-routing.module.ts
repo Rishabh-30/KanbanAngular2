fe6d7c5edbdc29da6a5ba911af0ca1804e08fcf4
Rishabh Pathak <rishabh.pathak30@hotmail.com>
2023-02-11 08:49:45 +0530

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,51 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AuthGuard } from './guards/auth.guard';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { CheckOtpComponent } from './components/check-otp/check-otp.component'; 
+import { SetNewPasswordComponent } from './components/set-new-password/set-new-password.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'forgot-password',
+    component:ForgotPasswordComponent
+  },
+  {
+    path:'home-page',
+    component:HomePageComponent
+  },
+  {
+    path:'check-otp',
+    component:CheckOtpComponent
+  },
+  {
+    path:'set-new-password',
+    component:SetNewPasswordComponent
+  },
+  {
+  path:'register',
+  component:RegisterComponent
+  },
+  {
+    path:'',
+    redirectTo:'home-page',
+    pathMatch:'full'
+
+  },
+  {
+    path:'admin',
+    // canActivate:[AuthGuard],
+    loadChildren:() => import('./modules/admin/admin.module').then((m)=>m.AdminModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
